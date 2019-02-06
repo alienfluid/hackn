d9e8fce1262679d4d84d7c12de1d296b5929add9
Farhan Ahmed <fah@instagram.com>
2019-02-06 10:20:10 -0500

    Added detail page info tile

--- lib/details.dart
+++ lib/details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'posts.dart';
+import 'hnutils.dart';
 
 class DetailScreen extends StatelessWidget {
   // Declare a field that holds the Todo
@@ -12,10 +13,65 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: <Widget>[Icon(Icons.comment, color: Colors.white), Icon(Icons.open_in_browser, color: Colors.white)],
+        actions: <Widget>[
+        ],
       ),
-      body: Text(post.title),
+      body: DetailTile(post: this.post),
     );
   }
+}
 
+class DetailTile extends StatelessWidget {
+  final PostWidget post;
+
+  DetailTile({Key key, @required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            Row(children: <Widget>[
+              Expanded(child: Text(this.post.title, textScaleFactor: 1.2))
+            ]),
+            Container(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(children: <Widget>[
+                  Text("authorname"),
+                  Spacer(flex: 4),
+                  Text(getHumanTime(this.post.time)),
+                  Spacer(flex: 4),
+                  Icon(Icons.thumb_up, color: Colors.grey),
+                  Container(
+                      padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                      child: Text(
+                        "345",
+                        textScaleFactor: 1.2,
+                      )),
+                  Spacer(),
+                  Icon(Icons.comment, color: Colors.grey),
+                  Container(
+                      padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                      child: Text("456", textScaleFactor: 1.2)),
+                ])),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: Row(children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                      child: Icon(Icons.open_in_browser)),
+                  Expanded(child: Text(shortenString(this.post.url))),
+                ])),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: Row(children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                      child: Icon(Icons.open_in_browser)),
+                  Expanded(child: Text(shortenString(getHNUrl(this.post.id)))),
+                ])),
+          ],
+        ));
+  }
 }
--- lib/hnutils.dart
+++ lib/hnutils.dart
@@ -1,11 +1,24 @@
-main () {
+import 'package:intl/intl.dart';
+
+main() {
   print(shortenString("This is a test of a long string"));
   print(shortenString("short string"));
 }
 
 String shortenString(String s) {
-  if (s.length > 40) {
-    return s.substring(0, 37) + "...";
-  } 
+  if (s.length > 48) {
+    return s.substring(0, 45) + "...";
+  }
   return s;
+}
+
+String getHumanTime(int epoch) {
+  DateTime d = new DateTime.fromMicrosecondsSinceEpoch(epoch);
+  var format = new DateFormat("yMd");
+  var dateString = format.format(d);
+  return dateString;
+}
+
+String getHNUrl(int id) {
+  return "https://news.ycombinator.com/item?id=" + id.toString();
 }
